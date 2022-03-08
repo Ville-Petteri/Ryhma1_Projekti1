651a20323bc98e558f04445eb0e83ea3711905b7
seppohto <olli.uronen@gmail.com>
2022-03-08 13:37:32 +0200

    valikko tehty

---
+++ vov-tuntikirjaus.py
@@ -0,0 +1,22 @@
+def vov_valikko():
+    print('VOV-tuntikirjaus')
+    print('1: Lisää tuntikirjaus')
+    print('2: Näytä Projektit')
+    print('3: Näytä kirjaukset')
+    print('4: Muokkaa kirjausta')
+    print('5: Näytä projekti')
+    print('6: Lopeta')
+    valinta = int(input('Syötä valinta: '))
+    if valinta == 1:
+        return
+    elif valinta == 2:
+        return
+    elif valinta == 3:
+        return
+    elif valinta == 4:
+        return
+    elif valinta == 5:
+        return
+    elif valinta == 5:
+        return
+    
