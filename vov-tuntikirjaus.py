c0b45e3dd42dd6ee09c0cf46110d7c04172b5eb9
seppohto <olli.uronen@gmail.com>
2022-03-09 10:14:41 +0200

    CRUD operaatiot lisätty

--- vov-tuntikirjaus.py
+++ vov-tuntikirjaus.py
@@ -1,6 +1,6 @@
-import datetime
 import queries
 
+#aloitus valikko
 def vov_valikko():
     while True:
         print('******************************')
@@ -13,82 +13,45 @@ def vov_valikko():
         print('5: Lopeta')
         valinta = int(input('Syötä valinta: '))
         if valinta == 1:
-            lisaa_tuntikirjaus()
+            queries.connect(valinta)
+            return
         elif valinta == 2 or valinta == 3 or valinta == 4:
                 valikko_crud(valinta)
         elif valinta == 5:
             break
 
+#CRUD valikot
 def valikko_crud(valinta):
-    muokattava=""
+    valikkonimi=""
     if(valinta == 2):
-        muokattava = "Käyttäjät"
+        valikkonimi = "Käyttäjät"
     elif(valinta == 3):
-        muokattava = "Projektit"
+        valikkonimi = "Projektit"
     elif(valinta == 4):
-        muokattava = "Kirjaukset"
-    print('------------------------------')
-    print(muokattava +' CRUD')
-    print('------------------------------')    
-    print('1:Lisää uusi')
-    print('2:Listaa')
-    print('3:Muokkaa')
-    print('4:Deletoi\n')
+        valikkonimi = "Tuntikirjaukset"
+    if(valinta == 2 or valinta == 3):
+        print('------------------------------')
+        print(valikkonimi +' CRUD')
+        print('------------------------------')    
+        print('1:Listaa')
+        print('2:Lisää uusi')
+        print('3:Muokkaa')
+        print('4:Deletoi\n')        
+        print('5:Takaisin\n')
+    elif(valinta == 4):
+        print('------------------------------')
+        print(valikkonimi +' CRUD')
+        print('------------------------------')    
+        print('1:Listaa')
+        print('2:Lisää uusi')
+        print('4:Deletoi\n')        
+        print('5:Takaisin\n')
     valinta_crud = int(input('Syötä valinta: '))
+    if valinta_crud == 5:
+        return
+    else:
+        queries.connect(valinta*10+valinta_crud)
 
-def lisaa_tuntikirjaus():
-    CurrentDate = datetime.date.today()
-    tanaanpaiva = str(CurrentDate.day) + "-" + str(CurrentDate.month) + "-" + str(CurrentDate.year)
-    aloituspvmaika=""
-    lopetuspvmaika=""
-
-    print('\nTänään on '+tanaanpaiva+"\n")
-
-    print("Tuntikirjaus")
-    
-    while True:
-        try:
-            print("Syötä aloituspvm muodossa DD-MM-YYYY")
-            aloituspvm = input("aloituspvm: ")
-            
-            print("Syötä aloitusaika muodossa HH:MM")
-            aloitusaika = input("aloitusaika: ")
-
-            aloituspvmaika = muodosta_date_pvm(aloituspvm+" "+aloitusaika)
-            break
-        except:
-            print("------------------------------")
-            print("\n VIRHE!!! VIRHE!!! VIRHE!!!\n")
-            print("Tarkista, että syötit pvm muodossa DD-MM-YYYY ja ajan muodossa HH:MM")
-            print("------------------------------\n")
-
-    while True:
-        try:
-            print("Syötä lopetuspvm muodossa DD-MM-YYYY")
-            lopetuspvm = input("lopetuspvm: ")
-            
-            print("Syötä lopetusaika muodossa HH:MM")
-            lopetusaika = input("lopetusaika: ")
-
-            lopetuspvmaika = muodosta_date_pvm(aloituspvm+" "+aloitusaika)
-            break
-        except:
-            print("------------------------------")
-            print("\n VIRHE!!! VIRHE!!! VIRHE!!!\n")
-            print("Tarkista, että syötit pvm muodossa DD-MM-YYYY ja ajan muodossa HH:MM")
-            print("------------------------------\n")    
-
-    print("Syötä projektinumero")
-    projektitieto = int(input("Projekti: "))
-
-    print("Lisää työselite")
-    selite = input("Selite:")
-
-    print(aloituspvm,aloitusaika,lopetusaika,lopetuspvm,projektitieto,selite)
-
-def muodosta_date_pvm(pvmaika):
-    date_muotoiltu_pvm = datetime.datetime.strptime(pvmaika,"%d-%m-%Y %H:%M")
-    return date_muotoiltu_pvm
-
+#ohjelman ajaminen
 if __name__ == '__main__':
     vov_valikko()
