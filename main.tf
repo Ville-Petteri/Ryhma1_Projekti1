644b43361126c6ca69433935171423020f3cc7d6
Viorika <andrijesh.viorika@gmail.com>
2022-03-09 15:59:03 +0200

    keyvault lisätty

--- main.tf
+++ main.tf
@@ -11,9 +11,16 @@ terraform {
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy = true
+    }
+  }
   subscription_id = var.subscription_id
   tenant_id       = var.tenant_id
+
+
+
 }
 
 resource "azurerm_resource_group" "rg" {
@@ -261,7 +268,38 @@ resource "azurerm_virtual_machine_scale_set" "rg" {
   }
 }
 
+#keyvault
 
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "rg" {
+  name                        = "${var.prefix}keyvault"
+  location                    = azurerm_resource_group.rg.location
+  resource_group_name         = azurerm_resource_group.rg.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "Get",
+    ]
+
+    secret_permissions = [
+      "Get",
+    ]
+
+    storage_permissions = [
+      "Get",
+    ]
+  }
+}
 
 
 
