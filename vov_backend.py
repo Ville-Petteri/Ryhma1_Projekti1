386975f60657ae73c8013057bbf1a19c7028d20c
ville-petteri <ville-petteri.saily@live.com>
2022-03-09 13:33:59 +0200

    ensimmäinen versio email-raportista

--- vov_backend.py
+++ vov_backend.py
@@ -1,6 +1,8 @@
 import psycopg2
 from config import config
 import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 import emailcredentials
 
 
@@ -10,9 +12,12 @@ def connect():
         
         con = psycopg2.connect(**config())
         cursor = con.cursor()
+        luo_taulu_kayttajat(cursor)
+        luo_taulu_projektit(cursor)
+        luo_taulu_tuntikirjaukset(cursor)
         #select_tuntikirjaukset(cursor)
         #select_tuntisumma(cursor)
-        #luo_taulu_tuntikirjaukset(cursor)
+        laheta_email(cursor)
         con.commit()
         cursor.close()
     except (Exception, psycopg2.DatabaseError) as error:
@@ -29,20 +34,23 @@ def luo_taulu_projektit(cursor):
     cursor.execute(SQL)
 
 def luo_taulu_tuntikirjaukset(cursor):
-    SQL='CREATE TABLE IF NOT EXISTS tuntikirjaukset (id SERIAL PRIMARY KEY,aloitus TIMESTAMP NOT NULL,lopetus TIMESTAMP NOT NULL,selite varchar(255),projektit_id INT,kayttajat_id INT,CONSTRAINT fk_projektit FOREIGN KEY (projektit_id) REFERENCES projektit(id),CONSTRAINT fk_kayttajat FOREIGN KEY(kayttajat_id) REFERENCES kayttajat(id));'
+    SQL='CREATE TABLE IF NOT EXISTS tuntikirjaukset (id SERIAL PRIMARY KEY,aloitus TIMESTAMP NOT NULL,lopetus TIMESTAMP NOT NULL,selite varchar(255),projektit_id INT,kayttajat_id INT,saa_tieto varchar(255), CONSTRAINT fk_projektit FOREIGN KEY (projektit_id) REFERENCES projektit(id),CONSTRAINT fk_kayttajat FOREIGN KEY(kayttajat_id) REFERENCES kayttajat(id));'
     cursor.execute(SQL)    
 
 
 def select_tuntikirjaukset(cursor):
+    palautus=""
     SQL = 'SELECT * FROM tuntikirjaukset;'
     cursor.execute(SQL)
     colnames= [desc[0] for desc in cursor.description]
-    print(colnames)
+    #print(colnames)
+    palautus=palautus+str(colnames)
     row =cursor.fetchone()
     while row is not None:
-        print(row)
+        #print(row)
+        palautus=palautus+str(row)
         row = cursor.fetchone()
-
+    return(palautus)
 def select_tuntisumma(cursor):
     SQL ='SELECT AGE(lopetus, aloitus) AS  kesto FROM tuntikirjaukset;'
     cursor.execute(SQL)
@@ -53,19 +61,30 @@ def select_tuntisumma(cursor):
         print(row)
         row = cursor.fetchone()
 
-def laheta_email():
+def laheta_email(cursor):
+    fromaddr = emailcredentials.emailuser
+    toaddr = emailcredentials.emailreceiver
+    msg = MIMEMultipart()
+    msg['From'] = fromaddr
+    msg['To'] = toaddr
+    msg['Subject'] = "Tuntiraportti"
+    body = select_tuntikirjaukset(cursor)
+    msg.attach(MIMEText(body, 'plain'))
+    
+    
+    
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.ehlo()
     server.starttls()
     server.ehlo()
     server.login(emailcredentials.emailuser, emailcredentials.emailpassword)
 
-    msg = "\nHello!" # The /n separates the message from the headers (which we ignore for this example)
-    server.sendmail(emailcredentials.emailuser, "ville-petteri.saily@awacademy.training", msg)
+    text = msg.as_string()
+    server.sendmail(fromaddr, toaddr, text)
 
 
 
 
 if __name__ == '__main__':
     #connect()
-    laheta_email()
+    connect()
