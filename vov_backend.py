f1c1e4d897cbb352e8baf537132f599ff1a48025
ville-petteri <ville-petteri.saily@live.com>
2022-03-09 11:03:08 +0200

    taulujen luonti, jos tauluja ei ole

--- vov_backend.py
+++ vov_backend.py
@@ -9,16 +9,30 @@ def connect():
         con = psycopg2.connect(**config())
         cursor = con.cursor()
         select_tuntikirjaukset(cursor)
-        #con.commit()
+        #select_tuntisumma(cursor)
+        #luo_taulu_tuntikirjaukset(cursor)
+        con.commit()
         cursor.close()
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
     finally:
         if con is not None:
             con.close()
+def luo_taulu_kayttajat(cursor):
+    SQL='CREATE TABLE IF NOT EXISTS kayttajat (id SERIAL PRIMARY KEY,name varchar(255) NOT NULL,email varchar(255));'
+    cursor.execute(SQL)
+
+def luo_taulu_projektit(cursor):
+    SQL='CREATE TABLE IF NOT EXISTS projektit (id SERIAL PRIMARY KEY,name varchar(255) NOT NULL,selite varchar(255));'
+    cursor.execute(SQL)
+
+def luo_taulu_tuntikirjaukset(cursor):
+    SQL='CREATE TABLE IF NOT EXISTS tuntikirjaukset2 (id SERIAL PRIMARY KEY,aloitus TIMESTAMP NOT NULL,lopetus TIMESTAMP NOT NULL,selite varchar(255),projektit_id INT,kayttajat_id INT,CONSTRAINT fk_projektit FOREIGN KEY (projektit_id) REFERENCES projektit(id),CONSTRAINT fk_kayttajat FOREIGN KEY(kayttajat_id) REFERENCES kayttajat(id));'
+    cursor.execute(SQL)    
+
 
 def select_tuntikirjaukset(cursor):
-    SQL = 'SELECT * FROM tuntikirjaukset;'
+    SQL = 'SELECT * FROM tuntikirjaukset2;'
     cursor.execute(SQL)
     colnames= [desc[0] for desc in cursor.description]
     print(colnames)
@@ -28,7 +42,16 @@ def select_tuntikirjaukset(cursor):
         row = cursor.fetchone()
 
 def select_tuntisumma(cursor):
-    SQL ='SELECT SUM() FROM table_name'
+    SQL ='SELECT AGE(lopetus, aloitus) AS  kesto FROM tuntikirjaukset;'
+    cursor.execute(SQL)
+    colnames= [desc[0] for desc in cursor.description]
+    print(colnames)
+    row =cursor.fetchone()
+    while row is not None:
+        print(row)
+        row = cursor.fetchone()
+
+
 
 
 
