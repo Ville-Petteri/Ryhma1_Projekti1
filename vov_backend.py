b8df48173442a87771c67c5d3bf77ac233cdd710
ville-petteri <ville-petteri.saily@live.com>
2022-03-10 16:00:52 +0200

    Alle tunnin ikäiset kirjaukset erilliseen tauluun

--- vov_backend.py
+++ vov_backend.py
@@ -18,9 +18,6 @@ def connect():
         luo_taulu_kayttajat(cursor)
         luo_taulu_projektit(cursor)
         luo_taulu_tuntikirjaukset(cursor)
-        #select_tuntisumma(cursor)
-        #tuntikirjaukset_nimilla(cursor)
-        #tuntikirjaukset_table(cursor)
         laheta_email(cursor)
         con.commit()
         cursor.close()
@@ -34,13 +31,15 @@ def connect():
 def luo_taulu_kayttajat(cursor):
     SQL='CREATE TABLE IF NOT EXISTS kayttajat (id SERIAL PRIMARY KEY,name varchar(255) NOT NULL,email varchar(255));'
     cursor.execute(SQL)
+
 #Funktio luo taulun projektit jos sitä ei ole
 def luo_taulu_projektit(cursor):
     SQL='CREATE TABLE IF NOT EXISTS projektit (id SERIAL PRIMARY KEY,name varchar(255) NOT NULL,selite varchar(255));'
     cursor.execute(SQL)
+
 #Funktio luo taulun tuntikirjausket, jos sitä ei ole
 def luo_taulu_tuntikirjaukset(cursor):
-    SQL='CREATE TABLE IF NOT EXISTS tuntikirjaukset (id SERIAL PRIMARY KEY,aloitus TIMESTAMP NOT NULL,lopetus TIMESTAMP NOT NULL,selite varchar(255),projektit_id INT,kayttajat_id INT,saa_tieto varchar(255), CONSTRAINT fk_projektit FOREIGN KEY (projektit_id) REFERENCES projektit(id),CONSTRAINT fk_kayttajat FOREIGN KEY(kayttajat_id) REFERENCES kayttajat(id));'
+    SQL='CREATE TABLE IF NOT EXISTS tuntikirjaukset (id SERIAL PRIMARY KEY,aloitus TIMESTAMP NOT NULL,lopetus TIMESTAMP NOT NULL,selite varchar(255),projektit_id INT,kayttajat_id INT,saa_tieto varchar(255),DateCreated TIMESTAMP NOT NULL DEFAULT NOW(), CONSTRAINT fk_projektit FOREIGN KEY (projektit_id) REFERENCES projektit(id),CONSTRAINT fk_kayttajat FOREIGN KEY(kayttajat_id) REFERENCES kayttajat(id));'
     cursor.execute(SQL)    
 
 #Funktio hakee ja palauttaa kaikkien tuntikirjauksien pituuksien summan Total nimellä
@@ -50,44 +49,38 @@ def select_tuntisumma(cursor):
     row =cursor.fetchone()
     return(row[0])
     
-#Funktio hakee ja palauttaa kirjaajan nimen,projektin nimen,aloitusjan ja keston sekä selitteen jokaisesta tuntikirjauksesta
-def tuntikirjaukset_nimilla(cursor):
-    palautus=""
-    SQL = 'SELECT kayttajat.name AS Käyttäjä,projektit.name AS Projekti,tuntikirjaukset.aloitus,tuntikirjaukset.lopetus - tuntikirjaukset.aloitus AS Kesto,tuntikirjaukset.selite,tuntikirjaukset.saa_tieto FROM tuntikirjaukset, projektit,kayttajat WHERE tuntikirjaukset.projektit_id=projektit.id AND tuntikirjaukset.kayttajat_id=kayttajat.id;'
-    cursor.execute(SQL)
-    colnames= [desc[0] for desc in cursor.description]
-    #print(colnames)
-    palautus=palautus+str(colnames) +"<br>"
-    row =cursor.fetchone()
-    palautus=palautus+f'{row[0]} {row[1]} {row[2]} {row[3]} {row[4]}'+"<br>"
-    
-    while row is not None:
-        #print(row)
-        palautus=palautus+f'{row[0]} {row[1]} {row[2]} {row[3]} {row[4]}'+"<br>"
-        row = cursor.fetchone()
-    #print(palautus)
-    return(palautus)
-
+#Funktio hakee ja palauttaa kaikki tuntikirjaukset ja hakee niille käyttäjä ja projektinimet toisista tableista palautusta muokataan html-tageilla
 def tuntikirjaukset_table(cursor):
     palautus="<table> <tr>"
     SQL = 'SELECT kayttajat.name AS Käyttäjä,projektit.name AS Projekti,tuntikirjaukset.aloitus,tuntikirjaukset.lopetus - tuntikirjaukset.aloitus AS Kesto,tuntikirjaukset.selite,tuntikirjaukset.saa_tieto FROM tuntikirjaukset, projektit,kayttajat WHERE tuntikirjaukset.projektit_id=projektit.id AND tuntikirjaukset.kayttajat_id=kayttajat.id;'
     cursor.execute(SQL)
-    #colnames= [desc[0] for desc in cursor.description]
-    #print(colnames)
     for desc in cursor.description:
-        #print(desc[0])
         palautus=palautus+ "<td>" +str(desc[0])+"</td>"
-    #palautus=palautus+str(colnames) +"<br>"
     palautus=palautus+"</tr>"
     row =cursor.fetchone()
     palautus=palautus+f'<tr><td>{row[0]} <td>{row[1]}</td> <td>{row[2]}</td> <td>{row[3]}</td> <td>{row[4]} </td><td>{row[5]}</td></tr>'
-    
     while row is not None:
-        #print(row)
         palautus=palautus+f'<tr><td>{row[0]} <td>{row[1]}</td> <td>{row[2]}</td> <td>{row[3]}</td> <td>{row[4]} </td><td>{row[5]}</td></tr>'
         row = cursor.fetchone()
     palautus=palautus+"</table>"
     return(palautus)
+
+#Funktio hakee ja palauttaa kaikki alle tunnin ikäiset tuntikirjaukset ja hakee niille käyttäjä ja projektinimet toisista tableista palautusta muokataan html-tageilla
+def tuntikirjaukset_table_age(cursor):
+    palautus="<table> <tr>"
+    SQL = "SELECT kayttajat.name AS Käyttäjä,projektit.name AS Projekti,tuntikirjaukset.aloitus,tuntikirjaukset.lopetus - tuntikirjaukset.aloitus AS Kesto,tuntikirjaukset.selite,tuntikirjaukset.saa_tieto FROM tuntikirjaukset, projektit,kayttajat WHERE tuntikirjaukset.projektit_id=projektit.id AND tuntikirjaukset.kayttajat_id=kayttajat.id AND NOW() - tuntikirjaukset.DateCreated < ( 1 ) * INTERVAL '1' hour;"
+    cursor.execute(SQL)
+    for desc in cursor.description:
+        palautus=palautus+ "<td>" +str(desc[0])+"</td>"
+    palautus=palautus+"</tr>"
+    row =cursor.fetchone()
+    palautus=palautus+f'<tr><td>{row[0]} <td>{row[1]}</td> <td>{row[2]}</td> <td>{row[3]}</td> <td>{row[4]} </td><td>{row[5]}</td></tr>'  
+    while row is not None:
+        palautus=palautus+f'<tr><td>{row[0]} <td>{row[1]}</td> <td>{row[2]}</td> <td>{row[3]}</td> <td>{row[4]} </td><td>{row[5]}</td></tr>'
+        row = cursor.fetchone()
+    palautus=palautus+"</table>"
+    return(palautus)
+   
 
 #Funktio luo ja palauttaa emailin body osuuden hakien sisällön funktioiden avulla
 def luo_email(cursor):
@@ -96,7 +89,9 @@ def luo_email(cursor):
     data1=select_tuntisumma(cursor)
     otsikko2="<br><h3> Kaikki tuntikirjaukset: </h3><br>"
     data2=tuntikirjaukset_table(cursor)
-    palautus=(f'{style}{otsikko1}{data1}{otsikko2}{data2}')
+    otsikko3="<br><h3> Uudet tuntikirjaukset: </h3><br>"
+    data3=tuntikirjaukset_table_age(cursor)
+    palautus=(f'{style}{otsikko1}{data1}{otsikko2}{data2}{otsikko3}{data3}')
     return(palautus)
 
 #Funktio lähettää emailin emailcredentials tiedostossa määritellystä ositteesta samassa tiedostossa määriteltyyn osoitteeseen
@@ -119,10 +114,5 @@ def laheta_email(cursor):
     server.sendmail(fromaddr, toaddr, text)
 
 
-
-
-
-
 if __name__ == '__main__':
-    #connect()
     connect()
